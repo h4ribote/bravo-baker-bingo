404d004855e00a54c201ba83e5e97b0e02e9d337
h4ribote <github@h4ribote.net>
2026-03-30 13:59:50 +0900

    カードサイズの変数化

--- main.py
+++ main.py
@@ -37,14 +37,16 @@ class Cell:
         return self.__str__()
 
 class Bingo:
-    def __init__(self):
+    def __init__(self, size: int):
+        self.size = size
+
         self.lines: list[Line] = []
         """判定用ライン"""
         self.card: list[list[Cell]] = []
         """ビンゴカード"""
         self.cell_dict: dict[int, Grid] = {}
         """カード逆引き"""
-        self.number_pool: list[int] = [i for i in range(1, 76)]
+        self.number_pool: list[int] = [i for i in range(1, size * 15 + 1)]
         self.history: list[int] = []
 
         self.bingo: int = 0
@@ -56,46 +58,47 @@ class Bingo:
     def init_lines(self):
         """判定用ラインの初期化"""
         self.lines = []
-        for line_x in range(0, 5): # 縦ライン
+        for line_x in range(0, self.size): # 縦ライン
             new_line: list[Grid] = []
-            for line_y in range(0, 5):
+            for line_y in range(0, self.size):
                 new_line.append(Grid(line_x, line_y))
             self.lines.append(Line(new_line.copy()))
 
-        for line_y in range(0, 5): # 横ライン
+        for line_y in range(0, self.size): # 横ライン
             new_line: list[Grid] = []
-            for line_x in range(0, 5):
+            for line_x in range(0, self.size):
                 new_line.append(Grid(line_x, line_y))
             self.lines.append(Line(new_line.copy()))
 
         line_lt_rb: list[Grid] = [] # Left top -> Right bottom
         line_lb_rt: list[Grid] = [] # Left bottom -> Right top
-        for i in range(0, 5): # クロスライン
+        for i in range(0, self.size): # クロスライン
             line_lt_rb.append(Grid(i, i))
-            line_lb_rt.append(Grid(i, 4 - i))
+            line_lb_rt.append(Grid(i, self.size - 1 - i))
         self.lines.append(Line(line_lt_rb.copy()))
         self.lines.append(Line(line_lb_rt.copy()))
 
     def init_card(self):
         """ビンゴカードの初期化"""
         self.card = []
-        for _ in range(5):
+        for _ in range(self.size):
             self.card.append([])
 
-        for line_x in range(0, 5):
-            numbers = random.sample(range(line_x*15 + 1, line_x*15 + 16), k=5)
-            for line_y in range(0, 5):
+        for line_x in range(0, self.size):
+            numbers = random.sample(range(line_x*15 + 1, line_x*15 + 16), k=self.size)
+            for line_y in range(0, self.size):
                 self.card[line_x].append(Cell(numbers[line_y]))
                 self.cell_dict[numbers[line_y]] = Grid(line_x, line_y)
 
         # Free
-        self.cell_dict.pop(self.card[2][2].number)
-        self.card[2][2].number, self.card[2][2].is_hit = 0, True
+        center = self.size // 2
+        self.cell_dict.pop(self.card[center][center].number)
+        self.card[center][center].number, self.card[center][center].is_hit = 0, True
 
     def print_card(self):
         """ビンゴカードの表示"""
-        for y in range(0, 5):
-            for x in range(5):
+        for y in range(0, self.size):
+            for x in range(self.size):
                 print(self.card[x][y], end="")
             print()
 
@@ -118,11 +121,11 @@ class Bingo:
             for grid in line.grids:
                 if self.card[grid.x][grid.y].is_hit:
                     hits += 1
-            if hits == 5:
+            if hits == self.size:
                 self.lines[i].is_bingo = True
                 self.bingo += 1
                 self.reach -= 1
-            elif hits == 4 and not self.lines[i].is_reach:
+            elif hits == (self.size - 1) and not self.lines[i].is_reach:
                 self.lines[i].is_reach = True
                 self.reach += 1
 
@@ -134,7 +137,7 @@ class Bingo:
         return drew
 
 def main():
-    bingo = Bingo()
+    bingo = Bingo(5)
     while bingo.number_pool:
         drew = bingo.play_turn()
         print(f"Ball[{len(bingo.history)}] = {drew}")
